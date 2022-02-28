47b0ca976776ac272ead615deeed8bd92fbe6938
ericmli <ericmli1334@gmail.com>
2022-02-28 14:58:12 -0300

    Vector minor and position.

---
+++ Main.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.Scanner;
+
+
+public class Main {
+    
+    public static void main (String [] args){
+        
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int X[] = new int [N];
+        int min = 0, position = 0; 
+        
+        for(int i = 0 ; i < N ; i++){
+            
+            X[i] = sc.nextInt();
+            if(X[i] < min){
+                min = X[i];
+                position = i;
+            }
+        }
+        System.out.println(min);
+        System.out.println(position);
+    }
+ }
